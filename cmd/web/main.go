b4c3a91f78100060dfa2156176acc8d35463cecf
Luqman <luqmanhafizimohdzawawi@gmail.com>
2023-11-05 14:24:17 +0800

    create custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
 	mux := http.NewServeMux()
 
@@ -19,7 +23,14 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.HandleFunc("/snippet/view", snippetView)
-	log.Printf("Starting at server %s...", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting at server %s...", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
